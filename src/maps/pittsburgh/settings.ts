2b121b7e6d0e716a920db43c2f06ef49798424b4
Nathan Tate <nathantate88@gmail.com>
2025-03-30 16:25:49 +0530

    Build the initial version of Pittsburgh.

---
+++ src/maps/pittsburgh/settings.ts
@@ -0,0 +1,25 @@
+import { GameKey } from "../../api/game_key";
+import { MapSettings, ReleaseStage } from "../../engine/game/map_settings";
+import { PittsburghFunkyBuilding } from "./build_cost";
+import {
+  PittsburghAllowedActions,
+  PittsburghBuildDiscountManager,
+} from "./commonwealth";
+import { map } from "./grid";
+
+export class PittsburghMapSettings implements MapSettings {
+  readonly key = GameKey.PITTSBURGH;
+  readonly name = "Pittsburgh";
+  readonly minPlayers = 3;
+  readonly maxPlayers = 3;
+  readonly startingGrid = map;
+  readonly stage = ReleaseStage.BETA;
+
+  getOverrides() {
+    return [
+      PittsburghFunkyBuilding,
+      PittsburghAllowedActions,
+      PittsburghBuildDiscountManager,
+    ];
+  }
+}
