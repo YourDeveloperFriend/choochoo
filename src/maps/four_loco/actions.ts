f166af1f31c2f4a1f3f31595c683238fcacad887
Chad DeShon <chad.deshon@gmail.com>
2026-04-16 12:08:12 -0500

    Review fixes: src/maps/four_loco/actions.ts

--- src/maps/four_loco/actions.ts
+++ src/maps/four_loco/actions.ts
@@ -1,6 +1,8 @@
 import { Set as ImmutableSet } from "immutable";
 import { AllowedActions } from "../../engine/select_action/allowed_actions";
 import { Action } from "../../engine/state/action";
+import { LocoAction } from "../../engine/move/loco";
+import { fail } from "../../utils/validate";
 
 /**
  * In 4 Loco, the Locomotive action is removed from the action selection phase.
@@ -9,5 +11,15 @@ import { Action } from "../../engine/state/action";
 export class FourLocoAllowedActions extends AllowedActions {
   getActions(): ImmutableSet<Action> {
     return super.getActions().remove(Action.LOCOMOTIVE);
+  }
+}
+
+/**
+ * Guards against any code path that tries to invoke the Locomotive action
+ * directly during the move phase.
+ */
+export class FourLocoLocoAction extends LocoAction {
+  validate(): void {
+    fail({ invalidArgument: "Locomotive action is not available on this map" });
   }
 }
