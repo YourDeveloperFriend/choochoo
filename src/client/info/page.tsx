09b5496d264674e2ee212bafeca23cc741fc1f41
Ian Haken <ihaken@netflix.com>
2026-05-03 19:09:35 -0700

    Add info on the karma system to info page.

--- src/client/info/page.tsx
+++ src/client/info/page.tsx
@@ -27,6 +27,58 @@ export function InfoPage() {
           at 50 income.
         </li>
       </ul>
+
+      <Header as="h1">Abandoned Games and Karma</Header>
+      <p>
+        When you join a game, you are expected to take your turns within the
+        length of time specified for the table and to play the game until it is
+        completed. There are a few situations where a game could end
+        prematurely:
+        <ul>
+          <li>
+            If all players who have not yet been eliminated agree to concede,
+            the game will end immediately. The current scores are used to
+            determine placement and no one is considered to have abandoned the
+            game.
+          </li>
+          <li>
+            You can choose to abandon a game, which causes it to end
+            immediately. This counts as an abandoned game which negatively
+            impacts your karma score.
+          </li>
+          <li>
+            If another player has exceeded the allowed length of time to take
+            their turn, any player at the table can kick that player from the
+            table. The immediately ends the game and counts as an abandoned game
+            for the kicked player, negatively impacts their karma score.
+          </li>
+        </ul>
+      </p>
+      <p>
+        Karma is a measure of how many games you complete to the end.
+        <ul>
+          <li>You start at 75 karma.</li>
+          <li>
+            You gain one karma for every (multiplayer) game you complete, to a maximum of 100.
+          </li>
+          <li>
+            You lose 10 karma for every game you abandon (whether voluntary or
+            due to a kick out).
+          </li>
+        </ul>
+      </p>
+      <p>
+        You can set restrictions for tables you create. You can set the minimum
+        required karma no higher than your own, minus 5. For example, if you have 100 karma,
+        you can make sure that only players with 95 or more can join your game!
+      </p>
+      <p>
+        Pay attention to the turn duration of tables when you join them to make
+        sure you are able to play games in the expected length of time. Failure
+        to do say can result in you getting kicked from the table which will
+        impact your karma and may prevent you from joining games in the future
+        if done too often!
+      </p>
     </Container>
   );
 }
