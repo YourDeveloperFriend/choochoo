da567905db6895124dcaa83852f0aa74fe66d792
Ian Haken <ihaken@netflix.com>
2026-09-11 10:58:50 -0700

    Revert Wyoming pay-to-block action and custom Engineer behavior. Update grid.

--- src/maps/wyoming/view_settings.ts
+++ src/maps/wyoming/view_settings.ts
@@ -1,22 +1,10 @@
-import * as React from "react";
-import { Phase } from "../../engine/state/phase";
-import { MapViewSettings } from "../view_settings";
-import { WyomingActionSelectorSummary } from "./disable-action-modal";
 import { WyomingRules } from "./rules";
 import { WyomingMapSettings } from "./settings";
+import { MapViewSettings } from "../view_settings";
 
 export class WyomingViewSettings
   extends WyomingMapSettings
   implements MapViewSettings
 {
   getMapRules = WyomingRules;
-
-  getActionSummary(
-    phase: Phase | undefined,
-  ): (() => React.ReactNode) | undefined {
-    if (phase === Phase.ACTION_SELECTION) {
-      return WyomingActionSelectorSummary;
-    }
-    return undefined;
-  }
 }
