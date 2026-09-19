62e1033cc381122d66bb474f12f60c71f5fdac49
Ian Haken <ihaken@netflix.com>
2026-09-18 17:08:47 -0700

    Fixup dark mode on tables.

--- src/maps/isle_of_wight/settings.ts
+++ src/maps/isle_of_wight/settings.ts
@@ -1,4 +1,5 @@
 import {
+  CORTEXBOMB,
   JACK,
   MapSettings,
   PlayerCountRating,
@@ -47,6 +48,7 @@ export class IsleOfWightMapSettings implements MapSettings {
   };
   readonly startingGrid = map;
   readonly stage = ReleaseStage.DEVELOPMENT;
+  readonly developmentAllowlist = [JACK, CORTEXBOMB];
 
   getOverrides() {
     return [
